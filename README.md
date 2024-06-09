c366dd1ddb11cb29a443fc2ab3a1f2078b0efc38
connorpop <connor1pope@gmail.com>
2024-06-09 15:32:57 +0100

    v1.02

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # test-javascript
- for testing purposes
+
+# for testing purposes
--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
-console.log("Hello, World!")
+/*
+let admin = '';
+let userName = '';
+
+userName = 'connorpop';
+admin = userName;
+
+console.log(admin);
+*/
+// testing creating variables and assigning values to them
+
+/*
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log(percentage)
+*/
+// testing strings
+
+console.log(9007199254740991 + 1); // 9007199254740992
+console.log(9007199254740991 + 2); // 9007199254740992
+
+let string1 = "hello!";
+let string2 = `hello! my name is ${string1}`;
+
+console.log(string2);
