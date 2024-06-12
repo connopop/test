b683da38e5693f6f0710ee7da5d3f08f7e41fc90
connorpop <connor1pope@gmail.com>
2024-06-12 19:48:50 +0100

    1.04

--- javascript.js
+++ javascript.js
@@ -1,30 +1,7 @@
-/*
-let admin = '';
-let userName = '';
+function showMessage() {
+    console.log( `Hello ${name}!` );
+  }
 
-userName = 'connorpop';
-admin = userName;
+let name = "Connor";
 
-console.log(admin);
-*/
-// testing creating variables and assigning values to them
-
-/*
-const MAX = 57;
-let actual = MAX - 13;
-let percentage = actual / MAX;
-console.log(percentage)
-*/
-// testing strings
-
-/*
-console.log(9007199254740991 + 1); // 9007199254740992
-console.log(9007199254740991 + 2); // 9007199254740992
-
-let string1 = "hello!";
-let string2 = `hello! my name is ${string1}`;
-
-console.log(string2);
-*/
-// testing conditions
-
+showMessage();
